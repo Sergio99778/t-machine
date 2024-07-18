ffa2a5feb4b6810b269c68eb1fcd0c40ff3a1449
Sergio99778 <52770473+Sergio99778@users.noreply.github.com>
2024-07-18 03:43:45 +0000

    chore: Add search functionality to TodoList component

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,28 @@ import { TodoCounter } from "./components/TodoCounter";
 import { TodoItem } from "./components/TodoItem";
 import { TodoList } from "./components/TodoList";
 import { TodoSearch } from "./components/TodoSearch";
+import { useState } from "react";
 
 const defaultTodos = [
   { text: "Cortar cebolla", completed: true },
-  { text: "Tomar el curso de intro a React", completed: false },
+  { text: "Tomar el curso de intro a React", completed: true },
   { text: "Llorar con la llorona", completed: false },
 ];
 
 function App() {
+  const [searchValue, setSearchValue] = useState('');
+  const [todos, setTodos] = useState(defaultTodos); 
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+  const todosBySearch = todos.filter(todo => todo.text.toLowerCase().includes(searchValue.toLowerCase()));
+
   return (
     <>
-      <TodoCounter total={1} completed={0}/>
-      <TodoSearch/>
+      <TodoCounter total={totalTodos} completed={completedTodos}/>
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue}/>
       <TodoList>
-        {defaultTodos.map((todo) => (
+        {todosBySearch.map((todo) => (
           <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
         ))}
       </TodoList>
--- src/components/TodoSearch.tsx
+++ src/components/TodoSearch.tsx
@@ -1,5 +1,5 @@
 import './TodoSearch.css'
 
-export function TodoSearch() {
-  return <input className="TodoSearch" type="text" placeholder="Todo search" />;
+export function TodoSearch(props: { searchValue: string, setSearchValue: (value: string) => void }) {
+  return <input className="TodoSearch" type="text" placeholder="Todo search" onChange={(e) => props.setSearchValue(e.target.value)} value={props.searchValue}/>
 }
